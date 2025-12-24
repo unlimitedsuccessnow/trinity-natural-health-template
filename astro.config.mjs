75b53a1bfa5ff2963a27884f9a3f4ba0032545b9
unlimitedsuccessnow <mrjaredjames@gmail.com>
2025-12-23 22:36:03 -0800

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.naturalhealthtrinity.com',
+	vite: {
+		build: {
+			cssCodeSplit: true,
+			assetsInlineLimit: 8192
+		}
+	}
 });
